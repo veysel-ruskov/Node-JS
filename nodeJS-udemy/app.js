4fe6ba60ba5b935fce6398c2ab2d74a2543d3300
veysel-ruskov98 <77992122+veysel-ruskov98@users.noreply.github.com>
2021-09-28 21:48:18 +0300

    add
    add project

---
+++ nodeJS-udemy/app.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var fs = require('fs');
+
+
+http.createServer(function (req, res) {
+
+    if (req.url === '/') {
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(__dirname + '/index.htm', 'utf8').pipe(res);
+    }
+
+    else if (req.url === '/api') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        var obj = {
+            firstname: 'John',
+            lastname: 'Doe'
+        };
+        res.end(JSON.stringify(obj));
+    }
+
+    else{res.writeHead(404);
+    res.end();
+    }
+
+
+
+}).listen(1337, '127.0.0.1');
--- nodeJS-udemy/projectWebServerNodeJS/webApp.js
+++ nodeJS-udemy/projectWebServerNodeJS/webApp.js
@@ -1,13 +1,27 @@
 var http = require('http');
 var fs = require('fs');
+const { version } = require('os');
 
 http.createServer(function (req, res) {
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    var html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
-    var message = 'Hello world...';
-    html = html.replace('{Message}', message);
+    if (req.url === '/') {
 
-    res.end(html);
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        var html = fs.createReadStream(__dirname + '/index.htm', 'utf8').pipe(res);
+    }
+
+    if (req.url === '/api') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        var obj = {
+            firstname: 'John',
+            lastname: 'Doe'
+        };
+        res.end(JSON.stringify(obj));
+    }
+
+    res.writeHead(404);
+    res.end();
+
+
 
 }).listen(1337, '127.0.0.1');
